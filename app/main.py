391b8d16ff8427da9d069859222e2025974ebf75
MatheusSetmaier <matheussetmaier590@gmail.com>
2026-06-16 21:07:02 -0300

    Rota pela URL
    Adicionada rota para receber cidades pela URL (temporário)

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 def home():
     return{'mensagem': 'WeatherCache API funcionando'}
+
+@app.get('/weather/{cidade}')
+def buscarClima(cidade: str):
+    return{'cidade': cidade}
