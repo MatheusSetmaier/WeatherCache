967b10ff7adbbddcf5114826eab86ba42c5b1474
MatheusSetmaier <matheussetmaier590@gmail.com>
2026-06-18 17:04:05 -0300

    Redis e cache
    Implementado cache de consultas utilizando Redis

--- app/redisClient.py
+++ app/redisClient.py
@@ -1,1 +1,7 @@
-# lógica de clima será implementada depois
+import redis
+
+redis_client = redis.Redis(
+    host = 'localhost',
+    port = 6379,
+    decode_responses = True
+)
--- app/weatherService.py
+++ app/weatherService.py
@@ -1,12 +1,22 @@
 import os
+import json
 import requests
 from dotenv import load_dotenv
+from app.redisClient import redis_client
 
 load_dotenv()
 
 API_KEY = os.getenv('WEATHER_API_KEY')
 
 def get_weather(cidade: str):
+
+    cache = redis_client.get(cidade)
+
+    if cache:
+        print('Dados retirados do cache')
+        return json.loads(cache)
+    print('Dados retirados da API')
+
     url = (
         f"https://weather.visualcrossing.com/"
         f"VisualCrossingWebServices/rest/services/timeline/"
@@ -14,4 +24,12 @@ def get_weather(cidade: str):
     )
 
     response = requests.get(url)
-    return response.json()
+
+    dados = response.json()
+
+    redis_client.set(
+        cidade,json.dumps(dados),
+        ex = 43200
+    )
+
+    return dados
