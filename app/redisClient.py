5619192892bb56cbeb0d3a2a793b0c83f7cb7237
MatheusSetmaier <matheussetmaier590@gmail.com>
2026-06-27 12:31:24 -0300

    Adiciona suporte ao Redis via URL

--- app/redisClient.py
+++ app/redisClient.py
@@ -1,8 +1,16 @@
 import os
 import redis
 
-redis_client = redis.Redis(
-    host = os.getenv('REDIS_HOST', 'localhost'),
-    port = int(os.getenv('REDIS_PORT', 6379)),
-    decode_responses = True
-)
+redis_url = os.getenv('REDIS_HOST')
+
+if redis_url and redis_url.startswith('redis://'):
+    redis_client = redis.from_url(
+        redis_url,
+        decode_responses = True
+    )
+else:
+    redis_client = redis.Redis(
+        host = os.getenv('REDIS_HOST', 'localhost'),
+        port = int(os.getenv('REDIS_PORT', 6379)),
+        decode_responses = True
+    )
